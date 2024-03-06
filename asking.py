0ab2373a761b24185b515c030467b30811f435ac
lukilampo <158604576+lukilampo@users.noreply.github.com>
2024-03-06 19:34:53 +0300

    Add files via upload

---
+++ asking.py
@@ -0,0 +1,8 @@
+
+# მომხმარებელს შემოატანინეთ ორი ბულიან ტიპის მნიშვნელობა(true, false), შემდეგ კი დაბეჭდეთ ერკანზე ეს მნიშვნელობები    
+# შედარებული ერთმანეთზე, ამისთვის გამოიყენეთ ლოგიკური ოპერატორები (and და or)
+
+first_question=bool(input("მოგწონს goa?: "))
+second_question=bool(input("მოგწონს სკოლა?: "))
+print(first_question or second_question)
+print(first_question and second_question)
